47bb97e3672f334d6e2704da23929fbcf56d1d06
Nhan Chung <brian166chung@gmail.com>
2021-03-05 11:05:58 -0800

    Finished lab 5 and started on lab 6

--- Lab 5/CAecology.py
+++ Lab 5/CAecology.py
@@ -24,8 +24,12 @@ ecologyDF = ecologyDF.dropna(axis="columns")
 # (2) Calculating mean values of various variables in each elevation band
 # (3) Creating a function for plotting the dataframe
 # (4) Create plots of how these various variables vary with raw elevation
-# (5) Create plots of how these variables vary with elevation bands
-# (6) Visualize how AET varies with biomass
+# (5) Create plots of how these variables vary with elevation values in
+# elevation bands
+# (6) Visualize how various variables vary with biomass (elevation band values)
+# (7) Visualize how various variables vary with biomass (raw elevation values)
+# (8) Make final figure of various variables with biomass
+
 
 # (1) Calculating elevation bands
 oldCols = ecologyDF.columns.tolist()
@@ -117,45 +121,154 @@ plotData("Elevation (m)", "2015 tree death", ecologyDF, [2, 4, 7],
 max and min temperatures. No obvious relationships with elevation can be seen
 with other variables'''
 
-# (5) Create plots of how these variables vary with elevation bands
+# (5) Create plots of how these variables vary with elevation values in
+# elevation bands
 elevBandFig = "Bands of elevation and ecological variables"
 py.figure(num=elevBandFig, figsize=(20, 15))
-plotData("Elevation bands", "Precip (mm/yr)", means, [2, 4, 1],
-         "Elevation band", "Precipitation (mm/yr)", elevBandFig,
+plotData("Elevation (m)", "Precip (mm/yr)", means, [2, 4, 1],
+         "Elevation (m)", "Precipitation (mm/yr)", elevBandFig,
          "Precipitation", ".b")
-plotData("Elevation bands", "Tmax (oC)", means, [2, 4, 2],
-         "Elevation band", "Mean annual maximum temperature (degrees Celsius)",
+plotData("Elevation (m)", "Tmax (oC)", means, [2, 4, 2],
+         "Elevation (m)", "Mean annual maximum temperature (degrees Celsius)",
          elevBandFig, "Mean annual maximum temperature", "or")
-plotData("Elevation bands", "Tmin (oC)", means, [2, 4, 3],
-         "Elevation band", "Mean annual minimum temperature (degrees Celsius)",
+plotData("Elevation (m)", "Tmin (oC)", means, [2, 4, 3],
+         "Elevation (m)", "Mean annual minimum temperature (degrees Celsius)",
          elevBandFig, "Mean annual minimum temperature", "og")
-plotData("Elevation bands", "AET", means, [2, 4, 4], "Elevation band",
+plotData("Elevation (m)", "AET", means, [2, 4, 4], "Elevation (m)",
          "Actual evapotranspiration (mm/yr)", elevBandFig,
          "Actual evapotranspiration", "^c")
-plotData("Elevation bands", "biomass", means, [2, 4, 5], "Elevation band",
+plotData("Elevation (m)", "biomass", means, [2, 4, 5], "Elevation (m)",
          "Biomass (tons C/ha)", elevBandFig, "Biomass", "ok")
-plotData("Elevation bands", "P_ET", means, [2, 4, 6], "Elevation band",
+plotData("Elevation (m)", "P_ET", means, [2, 4, 6], "Elevation (m)",
          "Runoff (mm/yr)", elevBandFig, "Runoff", "sm")
-plotData("Elevation bands", "2015 tree death", means, [2, 4, 7],
-         "Elevation band", "Tree death (unitless)", elevBandFig,
+plotData("Elevation (m)", "2015 tree death", means, [2, 4, 7],
+         "Elevation (m)", "Tree death (unitless)", elevBandFig,
          "2015 tree death", "1y")
 
-# (6) Visualize how AET varies with biomass
+# (6) Visualize how various variables vary with biomass (elevation band values)
+biomassBandsFig = "Vegetation biomass and various variables (elevation bands)"
+py.figure(num=biomassBandsFig, figsize=(20, 15))
+
+AETbandsRegress = linregress(means["biomass"], means["AET"])
+mAETbands = AETbandsRegress[0]
+bAETbands = AETbandsRegress[1]
+rAETbands = AETbandsRegress[2]
+r2AETbands = rAETbands**2
+means["Predicted AET"] = mAETbands*means["biomass"] + bAETbands
 biomassAET = "Vegetation biomass and AET"
-biomassAETregress = linregress(ecologyDF["biomass"], ecologyDF["AET"])
-m = biomassAETregress[0]
-b = biomassAETregress[1]
-r = biomassAETregress[2]
-r2 = r**2
-r2str = "R-squared: {0:.3f}".format(r2)
-ecologyDF["Predicted AET"] = m*ecologyDF["biomass"] + b
-print(r2)
-py.figure(num=biomassAET, figsize=(8, 6))
-plotData("biomass", "AET", ecologyDF, [1, 1, 1],
+plotData("biomass", "AET", means, [2, 2, 1],
          "Vegetation biomass (tons C/ha)", "Actual evapotranspiration (mm/yr)",
-         biomassAET, biomassAET, "og", "Original data")
-plotData("biomass", "Predicted AET", ecologyDF, [1, 1, 1],
+         biomassBandsFig, biomassAET, "og", "Original data")
+plotData("biomass", "Predicted AET", means, [2, 2, 1],
          "Vegetation biomass (tons C/ha)", "Actual evapotranspiration (mm/yr)",
-         biomassAET, biomassAET, "-m", "Regression")
-py.text(320, 210, r2str)
+         biomassBandsFig, biomassAET, "-m", "Regression")
+py.legend()
+
+TmaxBandsRegress = linregress(means["biomass"], means["Tmax (oC)"])
+mTmaxBands = TmaxBandsRegress[0]
+bTmaxBands = TmaxBandsRegress[1]
+rTmaxBands = TmaxBandsRegress[2]
+r2TmaxBands = rTmaxBands**2
+means["Predicted Tmax"] = mTmaxBands*means["biomass"] + bTmaxBands
+biomassTmax = "Vegetation biomass and maximum temperature"
+plotData("biomass", "Tmax (oC)", means, [2, 2, 2],
+         "Vegetation biomass (tons C/ha)",
+         "Maximum mean annual temperature (degrees Celsius)",
+         biomassBandsFig, biomassTmax, "or", "Original data")
+plotData("biomass", "Predicted Tmax", means, [2, 2, 2],
+         "Vegetation biomass (tons C/ha)",
+         "Maximum mean annual temperature (degrees Celsius)",
+         biomassBandsFig, biomassTmax, "-c", "Regression")
+py.legend()
+
+precipBandsRegress = linregress(means["biomass"], means["Precip (mm/yr)"])
+mPrecipBands = precipBandsRegress[0]
+bPrecipBands = precipBandsRegress[1]
+rPrecipBands = precipBandsRegress[2]
+r2PrecipBands = rPrecipBands**2
+means["Predicted Precip"] = mPrecipBands*means["biomass"] + bPrecipBands
+biomassPrecip = "Vegetation biomass and precipitation"
+plotData("biomass", "Precip (mm/yr)", means, [2, 2, 3],
+         "Vegetation biomass (tons C/ha)", "Mean annual precipitation (mm/yr)",
+         biomassBandsFig, biomassPrecip, "ob", "Original data")
+
+plotData("biomass", "Predicted Precip", means, [2, 2, 3],
+         "Vegetation biomass (tons C/ha)", "Mean annual precipitation (mm/yr)",
+         biomassBandsFig, biomassPrecip, "-y", "Regression")
+py.legend()
+
+# (7) Visualize how various variables vary with biomass (raw elevation values)
+biomassFig = "Vegetation biomass and various variables (raw elevation values)"
+py.figure(num=biomassFig, figsize=(20, 15))
+
+AETregress = linregress(ecologyDF["biomass"], ecologyDF["AET"])
+mAET = AETregress[0]
+bAET = AETregress[1]
+rAET = AETregress[2]
+r2AET = rAET**2
+ecologyDF["Predicted AET"] = mAET*ecologyDF["biomass"] + bAET
+plotData("biomass", "AET", ecologyDF, [2, 2, 1],
+         "Vegetation biomass (tons C/ha)", "Actual evapotranspiration (mm/yr)",
+         biomassFig, biomassAET, "og", "Original data")
+plotData("biomass", "Predicted AET", ecologyDF, [2, 2, 1],
+         "Vegetation biomass (tons C/ha)", "Actual evapotranspiration (mm/yr)",
+         biomassFig, biomassAET, "-m", "Regression")
+py.legend()
+
+TmaxRegress = linregress(ecologyDF["biomass"], ecologyDF["Tmax (oC)"])
+mTmax = TmaxRegress[0]
+bTmax = TmaxRegress[1]
+rTmax = TmaxRegress[2]
+r2Tmax = rTmax**2
+ecologyDF["Predicted Tmax"] = mTmax*ecologyDF["biomass"] + bTmax
+plotData("biomass", "Tmax (oC)", ecologyDF, [2, 2, 2],
+         "Vegetation biomass (tons C/ha)",
+         "Maximum mean annual temperature (degrees Celsius)",
+         biomassFig, biomassTmax, "or", "Original data")
+plotData("biomass", "Predicted Tmax", ecologyDF, [2, 2, 2],
+         "Vegetation biomass (tons C/ha)",
+         "Maximum mean annual temperature (degrees Celsius)",
+         biomassFig, biomassTmax, "-c", "Regression")
+py.legend()
+
+precipRegress = linregress(ecologyDF["biomass"], ecologyDF["Precip (mm/yr)"])
+mPrecip = precipRegress[0]
+bPrecip = precipRegress[1]
+rPrecip = precipRegress[2]
+r2Precip = rPrecip**2
+ecologyDF["Predicted Precip"] = mPrecip*ecologyDF["biomass"] + bPrecip
+plotData("biomass", "Precip (mm/yr)", ecologyDF, [2, 2, 3],
+         "Vegetation biomass (tons C/ha)", "Mean annual precipitation (mm/yr)",
+         biomassFig, biomassPrecip, "ob", "Original data")
+
+plotData("biomass", "Predicted Precip", ecologyDF, [2, 2, 3],
+         "Vegetation biomass (tons C/ha)", "Mean annual precipitation (mm/yr)",
+         biomassFig, biomassPrecip, "-y", "Regression")
+py.legend()
+
+# (8) Make final figure of various variables with biomass
+biomassFinal = "Vegetation biomass and various variables"
+py.figure(num=biomassFinal, figsize=(20, 15))
+
+plotData("biomass", "AET", ecologyDF, [2, 2, 1],
+         "Vegetation biomass (tons C/ha)", "Actual evapotranspiration (mm/yr)",
+         biomassFinal, biomassAET, "og", "Original data")
+plotData("biomass", "Predicted AET", ecologyDF, [2, 2, 1],
+         "Vegetation biomass (tons C/ha)", "Actual evapotranspiration (mm/yr)",
+         biomassFinal, biomassAET, "-m", "Regression")
+py.legend()
+
+# uses elevation band values
+biomassTmaxBands = "Biomass and maximum temperature (elevation bands)"
+plotData("Tmax (oC)", "biomass", means, [2, 2, 2],
+         "Maximum mean annual temperature (degrees Celsius)",
+         "Vegetation biomass (tons C/ha)",
+         biomassFinal, biomassTmaxBands, "or")
+
+plotData("biomass", "Precip (mm/yr)", ecologyDF, [2, 2, 3],
+         "Vegetation biomass (tons C/ha)", "Mean annual precipitation (mm/yr)",
+         biomassFinal, biomassPrecip, "ob", "Original data")
+plotData("biomass", "Predicted Precip", ecologyDF, [2, 2, 3],
+         "Vegetation biomass (tons C/ha)", "Mean annual precipitation (mm/yr)",
+         biomassFinal, biomassPrecip, "-y", "Regression")
 py.legend()
---
+++ Lab 6/CAfires.py
@@ -0,0 +1,281 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  3 17:06:06 2021
+
+@author: Brian Chung
+Analyzes time-series burned area data in California
+"""
+import pandas as pd
+import os
+import matplotlib.pyplot as py
+from pathlib import Path
+from scipy.stats import linregress
+import numpy as np
+
+# %%
+# Reading in data
+cwd = Path(os.getcwd())
+folders = os.listdir(cwd)
+firePerimeterFolder = cwd/"Fire Perimeter"
+firePerimeterFiles = os.listdir(firePerimeterFolder)
+fireHistoryPath = firePerimeterFolder/'Fire history.xlsx'
+fireHistory = pd.read_excel(io=fireHistoryPath)
+
+# %%
+# Purpose: Cleaning data
+# Tasks:
+# (1) Dropping unnecessary columns
+# (2) Converting area from square meters to hectares
+# (3) Filtering out any data before 1900. Data before 1900 were sparse with
+# data present for the years 1878, 1895, 1896, 1898
+
+# (1) Dropping unnecessary columns
+fireHistory = fireHistory.dropna(axis="index", subset=["YEAR_", "Shape_Area"])
+'''It seems like dropping NA rows based on "YEAR_" is enough, no need to
+add in "Shape_Area", although adding in "Shape_Area" doesn't change the
+eventual number of rows'''
+fireHistory = fireHistory[fireHistory["STATE"] == "CA"]
+oldColumns = fireHistory.columns.tolist()
+columnsToRemove = ["OBJECTID", "STATE", "AGENCY", "UNIT_ID", "FIRE_NAME",
+                   "INC_NUM", "COMMENTS", "REPORT_AC", "FIRE_NUM", "C_METHOD",
+                   "Shape_Length"]
+fireHistory = fireHistory.drop(labels=columnsToRemove, axis="columns")
+newColumns = fireHistory.columns.tolist()
+
+# (2) Converting area from square meters to hectares
+fireHistory["Shape_Area"] = fireHistory["Shape_Area"]/10000
+
+# (3) Filtering out any data before 1900
+fireHistory = fireHistory[fireHistory["YEAR_"] >= 1900]
+# %%
+# Making yearly calculations
+groupByObject = fireHistory.groupby("YEAR_")
+totalBurnedArea = groupByObject["Shape_Area"].sum()
+avgFireSize = groupByObject["Shape_Area"].mean()
+numOfFires = groupByObject["YEAR_"].count()
+
+# To get the most common causes each year, that's a bit difficult, so...
+causes = fireHistory.groupby(["YEAR_", "CAUSE"])["YEAR_"].count()
+# This counts the number of fires by their causes each year
+
+# %%
+# Making plots
+# Tasks:
+# (1) Write a function to plot simple series with only 1 index
+# (2) Plot total and mean burned area and the number of fires against time
+
+
+# (1) Write a function to plot simple series with only 1 index
+
+
+def plotSimpleSeries(figTitle, figSize, subplotTitle, subplotOrder, series,
+                     xAxis, yAxis, markers, subplotLegend=None):
+    """
+    Plots time-series data from a simple series with only 1 index. Can make 1
+    or multiple subplots on 1 figure.
+
+    Parameters
+    ----------
+    figTitle : str
+        The title of the overall figure to be plotted. When called with
+        py.figure(), the figure with this title is either constructed from
+        scratch if not existing or activated if already created.
+    figSize : tuple of 2 integers
+        Tuple that specifies the width and height of the figure in the format
+        of (length, width).
+    subplotTitle : str
+        The title of the subplot to be plotted. When called with py.plot(),
+        will either create a new subplot with this title or activate an
+        existing subplot for modifications
+    subplotOrder : list of 3 integers
+        Specifies the order of a subplot in the order of
+        [row, column, subplot position]
+    series : Pandas series
+        The Pandas series for which time-series data will be plotted
+    xAxis : str
+        The title of the x-axis
+    yAxis : str
+        The title of the y-axis
+    markers : str
+        The marker type and color of the data to be plotted
+    subplotLegend : str
+        What will be displayed in the subplot legend should the subplot have
+        a legend
+
+    Returns
+    -------
+    None.
+
+    """
+    py.figure(num=figTitle, figsize=figSize)
+    py.subplot(subplotOrder[0], subplotOrder[1], subplotOrder[2])
+    py.title(subplotTitle)
+    py.xlabel(xAxis)
+    py.ylabel(yAxis)
+    py.plot(series.index, series, markers, label=subplotLegend)
+    if type(subplotLegend) is not None:
+        py.legend()
+    return
+
+
+# (2) Plot total and mean burned area and the number of fires against time
+areaFireNumFig = "Burned area and number of fires time series"
+areaTimeSeries = "Burned area"
+fireNumTimeSeries = "Number of fires"
+meanFireSize = "Mean fire size"
+
+plotSimpleSeries(areaFireNumFig, (20, 10), areaTimeSeries, [2, 2, 1],
+                 totalBurnedArea, "Year", "Burned Area (ha)", "o-r",
+                 "Total burned area")  # Total burned area
+plotSimpleSeries(areaFireNumFig, (20, 10), areaTimeSeries, [2, 2, 1],
+                 avgFireSize, "Year", "Burned Area (ha)",
+                 ".-k", "Mean fire size")  # Mean burned area per fire
+plotSimpleSeries(areaFireNumFig, (20, 10), fireNumTimeSeries, [2, 2, 2],
+                 numOfFires, "Year", "Number of fires", "o-g",
+                 "Number of fires")  # Number of fires
+plotSimpleSeries(areaFireNumFig, (20, 10), meanFireSize, [2, 2, 3],
+                 avgFireSize, "Year", "Mean fire size (ha)",
+                 "o-k", "Mean fire size")  # Mean burned area per fire
+# %%
+# Purpose: Perform and plot linear regressions
+# Tasks:
+# (1) Perform time-series linear regressions of total burned area, average fire
+# size, and number of fires and linear regressions of total burned area vs
+# fire size and total area vs number of fires
+# (2) Plot time-series linear regressions of all on original time-series
+# (3) Create a function to plot 2 numpy arrays, lists, or Pandas series
+# (4) Plot relationships between total burned area, number of fires, and fire
+# size
+
+
+# (1) Perform linear regressions of total burned area, average fire size, and
+# number of fires
+totalBurnedRegress = linregress(totalBurnedArea.index, totalBurnedArea)
+r2total = totalBurnedRegress[2]**2
+r2strTotal = "R-squared: {0:.3f}".format(r2total)
+fireSizeRegress = linregress(avgFireSize.index, avgFireSize)
+r2fireSize = fireSizeRegress[2]**2
+r2strFireSize = "R-squared: {0:.3f}".format(r2fireSize)
+fireNumRegress = linregress(numOfFires.index, numOfFires)
+r2fireNum = fireNumRegress[2]**2
+r2strFireNum = "R-squared: {0:.3f}".format(r2fireNum)
+totalVsFireNum = linregress(numOfFires, totalBurnedArea)
+totalVsSize = linregress(avgFireSize, totalBurnedArea)
+years = np.array(totalBurnedArea.index.tolist())
+
+# (2) Plot time-series linear regressions of all on original time-series
+totalBurnedEstimate = totalBurnedRegress[0]*years + totalBurnedRegress[1]
+py.subplot(2, 2, 1)
+py.plot(years, totalBurnedEstimate, "-m", label="Total burned area regression")
+# py.legend()
+py.text(1898, 490000, r2strTotal)
+fireNumEstimate = fireNumRegress[0]*years + fireNumRegress[1]
+py.subplot(2, 2, 2)
+py.plot(years, fireNumEstimate, "-m", label="Number of fires regression")
+# py.legend()
+py.text(1898, 500, r2strFireNum)
+fireSizeEstimate = fireSizeRegress[0]*years + fireSizeRegress[1]
+py.subplot(2, 2, 3)
+py.plot(years, fireSizeEstimate, "-m", label="Fire size regression")
+py.legend()
+py.text(1898, 1350, r2strFireSize)
+
+# (3) Create a function to plot 2 numpy arrays, lists, or Pandas series
+
+
+def plot2Vars(figTitle, figSize, subplotTitle, subplotOrder, xValues, yValues,
+              xTitle, yTitle, markers, labelLegend=None,
+              r=None, r2Coords=None):
+    """
+    Plots 2 numpy arrays or lists or Pandas series. Can plot on a figure with
+    a single subplot or a figure with multiple subplots. Can plot on existing
+    plots or create new plots altogether.
+
+    Parameters
+    ----------
+    figTitle : str
+        The title of the overall figure to be plotted. When called with
+        py.figure(), the figure with this title is either constructed from
+        scratch if not existing or activated if already created.
+    figSize : tuple of 2 integers
+        Tuple that specifies the width and height of the figure in the format
+        of (length, width).
+    subplotTitle : str
+        The title of the subplot to be plotted. When called with py.plot(),
+        will either create a new subplot with this title or activate an
+        existing subplot for modifications
+    subplotOrder : list of 3 integers
+        Specifies the order of a subplot in the order of
+        [row, column, subplot position]
+    xValues : 1-D Numpy array, list of integers or floats, or Pandas series
+        The values to be plotted on the x-axis
+    yArray : 1-D Numpy array, list of integers or floats, or Pandas series
+        The values to be plotted on the y-axis
+    xTitle : str
+        The title of the x-axis
+    yTitle : str
+        The title of the y-axis
+    markers : str
+        The marker type and color of the data to be plotted
+    labelLegend : str, OPTIONAL
+        What will be displayed in the subplot legend should the subplot have
+        a legend. Default is None because this is optional
+    r : float, OPTIONAL
+        The correlation coefficient between the x & y values
+    r2Coords : list of 2 numbers, OPTIONAL
+        r2 will be annotated onto a subplot. This list represents the
+        [x, y] coordinates of the left-hand side of the annotation that
+        contains r2.
+
+    Returns
+    -------
+    None.
+
+    """
+    py.figure(num=figTitle, figsize=figSize)
+    py.subplot(subplotOrder[0], subplotOrder[1], subplotOrder[2])
+    py.title(subplotTitle)
+    py.xlabel(xTitle)
+    py.ylabel(yTitle)
+    py.plot(xValues, yValues, markers, label=labelLegend)
+    if type(labelLegend) is not None:
+        py.legend()
+    if type(r) == np.float64:
+        r = r**2
+        r2str = "R-squared: {0:.3f}".format(r)
+        py.text(r2Coords[0], r2Coords[1], r2str)
+    return
+
+
+# (4) Plot relationships between total burned area, number of fires, and fire
+# size
+totalBurnedAreaFig = "Components of total burned area"
+totalVsNumEstimate = totalVsFireNum[0]*numOfFires + totalVsFireNum[1]
+numOfFiresSubplot = "Total burned area vs number of fires"
+plot2Vars(totalBurnedAreaFig, (20, 15), numOfFiresSubplot, [1, 2, 1],
+          numOfFires, totalBurnedArea, "Number of fires per year",
+          "Total burned area per year (ha)", "og", "Original data")
+plot2Vars(totalBurnedAreaFig, (20, 15), numOfFiresSubplot, [1, 2, 1],
+          numOfFires, totalVsNumEstimate, "Number of fires per year",
+          "Total burned area per year (ha)", "-m", "Regression",
+          totalVsFireNum[2], [500, 600000])
+
+totalVsSizeEstimate = totalVsSize[0]*avgFireSize + totalVsSize[1]
+numOfFiresSubplot = "Total burned area vs fire size"
+plot2Vars(totalBurnedAreaFig, (20, 15), numOfFiresSubplot, [1, 2, 2],
+          avgFireSize, totalBurnedArea, "Mean fire size per year (ha)",
+          "Total burned area per year (ha)", "og", "Original data")
+plot2Vars(totalBurnedAreaFig, (20, 15), numOfFiresSubplot, [1, 2, 2],
+          avgFireSize, totalVsSizeEstimate, "Mean fire size per year (ha)",
+          "Total burned area per year (ha)", "-m", "Regression",
+          totalVsSize[2], [0, 600000])
+# %%
+# Purpose: Analyzing how the causes of wildfires change over time
+# Tasks:
+# (1) Calculating the fire size and total burned area by fire causes annually
+# (2) Wrangling the fire size, total burned area, and number of fires by
+# causes into a single dataframe
+# (3) Distinguishing between all human causes and natural causes (lightning)
+# (4) Performing regressions over time of each individual cause and the
+# combined human & natural causes
+# (5) Visualizing the causes over time
