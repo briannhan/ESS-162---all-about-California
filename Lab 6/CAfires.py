065d6f534e1c1e36e418a464bd16c68c3513f4cb
Nhan Chung <brian166chung@gmail.com>
2021-03-06 22:24:06 -0800

    Completed Python analysis of lab 6 (probably)
    I analyzed the causes of wildfires

--- Lab 6/CAfires.py
+++ Lab 6/CAfires.py
@@ -135,7 +135,8 @@ plotSimpleSeries(areaFireNumFig, (20, 10), fireNumTimeSeries, [2, 2, 2],
                  "Number of fires")  # Number of fires
 plotSimpleSeries(areaFireNumFig, (20, 10), meanFireSize, [2, 2, 3],
                  avgFireSize, "Year", "Mean fire size (ha)",
-                 "o-k", "Mean fire size")  # Mean burned area per fire'''
+                 "o-k", "Mean fire size")  # Mean burned area per fire
+py.savefig(areaFireNumFig)'''
 # %%
 # Purpose: Perform and plot linear regressions
 # Tasks:
@@ -149,7 +150,8 @@ plotSimpleSeries(areaFireNumFig, (20, 10), meanFireSize, [2, 2, 3],
 
 
 # (1) Perform linear regressions of total burned area, average fire size, and
-# number of fires
+# number of fires over time as well as total burned area vs the other 2
+# variables
 totalBurnedRegress = linregress(totalBurnedArea.index, totalBurnedArea)
 r2total = totalBurnedRegress[2]**2
 r2strTotal = "R-squared: {0:.3f}".format(r2total)
@@ -179,7 +181,8 @@ fireSizeEstimate = fireSizeRegress[0]*years + fireSizeRegress[1]
 py.subplot(2, 2, 3)
 py.plot(years, fireSizeEstimate, "-m", label="Fire size regression")
 py.legend()
-py.text(1898, 1350, r2strFireSize)'''
+py.text(1898, 1350, r2strFireSize)
+py.savefig(areaFireNumFig)'''
 
 # (3) Create a function to plot 2 numpy arrays, lists, or Pandas series
 
@@ -245,6 +248,7 @@ def plot2Vars(figTitle, figSize, subplotTitle, subplotOrder, xValues, yValues,
         r = r**2
         r2str = "R-squared: {0:.3f}".format(r)
         py.text(r2Coords[0], r2Coords[1], r2str)
+    py.savefig(figTitle)
     return
 
 
@@ -280,8 +284,6 @@ plot2Vars(totalBurnedAreaFig, (20, 15), numOfFiresSubplot, [1, 2, 2],
 # (3) Distinguishing between all human causes and natural causes (lightning)
 # (4) Performing regressions over time of each individual cause and the
 # combined human & natural causes
-#   (4a) Creating a function to do the regression
-#   (4b) Call the function on the causes dataframes
 # (5) Visualizing the causes over time
 
 # (1) Calculating the fire size and total burned area by fire causes annually
@@ -333,19 +335,204 @@ totalAbyCause = totalAbyCause.rename(mapper=causesDict, axis="columns")
 sizeByCause["Manmade"] = totalAbyCause["Manmade"]/causesNum["Manmade"]
 sizeByCause = sizeByCause.fillna(value=0, axis="columns")
 sizeByCause["Natural"] = sizeByCause[1]
-sizeByCause["Miscellaneous/Unknown"] = sizeByCause[9] + sizeByCause[14]
+sizeByCause["Miscellaneous/Unknown"] = (totalAbyCause["Miscellaneous/Unknown"]
+                                        / causesNum["Miscellaneous/Unknown"])
 sizeByCause = sizeByCause.rename(mapper=causesDict, axis=1)
 causesColumns = causesNum.columns.tolist()[1:]
 
 # (4) Performing regressions over time of each individual cause and the
 # combined human & natural causes
-#   (4a) Creating a function to do the regression
-for cause in causesColumns:
-    regress = linregress(causesNum["YEAR_"], causesNum[cause])
-    indexList = causesDescribed[causesDescribed["Description"] == cause].index.tolist()
-    print(indexList)
-    '''causesDescribed[indexVal, "m_fireNum"] = regress[0]
-    causesDescribed[indexVal, "b_fireNum"] = regress[1]
-    causesDescribed[indexVal, "r_fireNum"] = regress[2]
-    causesDescribed[indexVal, "r2_fireNum"] = regress[2]**2
-    causesDescribed[indexVal, "p_fireNum"] = regress[3]'''
+
+
+def causesRegress(data, dataCategory):
+    """
+    Performs linear regression to analyze how the causes vary over time. It
+    adds columns to the causesDescribed dataframe that contains regression
+    information. This function will be used to analyze how the number of fires,
+    fire size, and the total burned area vary by the cause of fire and
+    over time.
+
+    Parameters
+    ----------
+    data : Pandas dataframe
+        The dataframe that will be used as the source of data. This dataframe
+        either contains data on the number of fires, fire size, or the total
+        burned area, all by cause. Linear regression will be performed on this
+        dataframe.
+    dataCategory : str
+        Indicates the category of the data to be analyzed, which will be either
+        the number of fires, mean fire size, or the total burned area, all by
+        cause. This string will be added to the new columns in the
+        causesDescribed dataframe.
+
+    Returns
+    -------
+    None.
+
+    """
+    mName = "m_{}".format(dataCategory)
+    bName = "b_{}".format(dataCategory)
+    rName = "r_{}".format(dataCategory)
+    r2Name = "r2_{}".format(dataCategory)
+    pName = "p_{}".format(dataCategory)
+    for cause in causesColumns:
+        regress = linregress(data["YEAR_"], data[cause])
+        ind = causesDescribed[causesDescribed["Description"] == cause].index
+        indexVal = ind.tolist()[0]
+        causesDescribed.loc[indexVal, mName] = regress[0]
+        causesDescribed.loc[indexVal, bName] = regress[1]
+        causesDescribed.loc[indexVal, rName] = regress[2]
+        causesDescribed.loc[indexVal, r2Name] = regress[2]**2
+        causesDescribed.loc[indexVal, pName] = regress[3]
+    return
+
+
+causesRegress(causesNum, dataCategory="fireNum")
+causesRegress(totalAbyCause, dataCategory="totalBurnedArea")
+causesRegress(sizeByCause, dataCategory="fireSize")
+
+# (5) Visualizing the causes over time
+causesFig = "All causes over time"
+causesNumSubplot = "Number of fires by cause over time"
+xAxis = "Year"
+yAxis = "Number of fires in a year"
+causesNotToPlot = ["Miscellaneous", "Unknown/Unidentified",
+                   "Miscellaneous/Unknown"]
+causesMarkers = [".-b", ".-g", ".-r", ".-c", ".-m", ".-y", ".-k", "o--b",
+                 "o--g", "o--r", "o--c", "o--m", "o--y", "o--k", "1:b", "1:g",
+                 "blank", "1:r", "1:c", "1:m", "1:y", "1:k"]
+causeCode = causesDescribed["Cause Code"].tolist()
+for code in causeCode:
+    indexRow = causesDescribed[causesDescribed["Cause Code"] == code].index
+    indexVal = indexRow.tolist()[0]
+    causesDescribed.loc[indexVal, "Raw data markers"] = causesMarkers[code - 1]
+causesNumRecent = causesNum[causesNum["YEAR_"] >= 1950]
+sizeByCauseRecent = sizeByCause[sizeByCause["YEAR_"] >= 1950]
+totalAbyCauseRecent = totalAbyCause[totalAbyCause["YEAR_"] >= 1950]
+
+
+def plotCauses(fireNumCauses, sameCauses, regressDisp, figName, period,
+               fireSizeCauses=None, totalAreaCauses=None):
+    """
+    Each call of this function will plot a single figure with 3 subplots. Each
+    subplot is a time series of either the number of fires, fire size, or the
+    total burned area.
+
+    Parameters
+    ----------
+    fireNumCauses : list of str
+        The causes in the causesNum dataframe to plot in the first subplot.
+    sameCauses : str
+        Values are either "Y" for "yes" or "N" for "no". This essentially
+        specifies whether the list of causes to plot for the first subplot
+        should be used for the remaining subplots. If "Y", then the causes
+        that were used to plot the first subplot will also be used to plot
+        the remaining subplots. If "N", then fireSizeCauses and totalAreaCauses
+        must be specified.
+    fireSizeCauses : str, optional
+        The causes that will be used to plot the second subplot, which is the
+        subplot on mean fire size per year. If sameCauses is "Y", then this
+        argument and the totalAreaCauses argumennt will be set to be equal to
+        fireNumCauses, and so the subplot on mean fire size will plot the same
+        causes as the other 2 subplots. The default is None.
+    totalAreaCauses : str, optional
+        The causes that will be used to plot the third subplot, which is the
+        subplot on total burned area per year. If sameCauses is "Y", then this
+        argument and the fireSizeCauses argumennt will be set to be equal to
+        fireNumCauses, and so the subplot on total burned area will plot the
+        same causes as the other 2 subplots. The default is None.
+    regressDisp : str
+        Values will be either "Y" for "yes" or "N". Asks whether the user wants
+        to plot regression lines for each cause on each subplot. If "Y", then
+        this function will plot regression lines. If "N", then this function
+        will not plot the regression lines.
+    figName : str
+        The name of the figure.
+    period : str
+        Values will be "all" or "recent". Meant to specify the period of
+        analysis in the subplots so that the subplots only contain data within
+        this period. "all" means that all the data from 1900 to 2019 will be
+        included, while "recent" will only include data from 1950 to 2019.
+
+    Returns
+    -------
+    None.
+
+    """
+    if sameCauses == "Y":
+        fireSizeCauses = fireNumCauses
+        totalAreaCauses = fireNumCauses
+
+    if period == "all":
+        numDF = causesNum
+        sizeDF = sizeByCause
+        totalAreaDF = totalAbyCause
+    elif period == "recent":
+        numDF = causesNumRecent
+        sizeDF = sizeByCauseRecent
+        totalAreaDF = totalAbyCauseRecent
+
+    for cause in fireNumCauses:
+        ind = causesDescribed[causesDescribed["Description"] == cause].index
+        markerFmt = causesDescribed.loc[ind, "Raw data markers"].tolist()[0]
+        plot2Vars(figName, (20, 15), "Number of fires in a year", [2, 2, 1],
+                  numDF["YEAR_"], numDF[cause], "Year",
+                  "Number of fires in a year", markers=markerFmt,
+                  labelLegend=cause)
+        if regressDisp == "Y":
+            m = causesDescribed.loc[ind, "m_fireNum"]
+            b = causesDescribed.loc[ind, "b_fireNum"]
+            predicted = m*years + b
+            py.plot(numDF["YEAR_"], predicted, markerFmt, label=cause)
+    for cause in fireSizeCauses:
+        ind = causesDescribed[causesDescribed["Description"] == cause].index
+        markerFmt = causesDescribed.loc[ind, "Raw data markers"].tolist()[0]
+        plot2Vars(figName, (20, 15), "Mean fire size", [2, 2, 2],
+                  sizeDF["YEAR_"], sizeDF[cause], "Year",
+                  "Mean fire size (ha)", markers=markerFmt, labelLegend=cause)
+        if regressDisp == "Y":
+            m = causesDescribed.loc[ind, "m_fireSize"]
+            b = causesDescribed.loc[ind, "b_fireSize"]
+            predicted = m*years + b
+            py.plot(sizeDF["YEAR_"], predicted, markerFmt, label=cause)
+    for cause in totalAreaCauses:
+        ind = causesDescribed[causesDescribed["Description"] == cause].index
+        markerFmt = causesDescribed.loc[ind, "Raw data markers"].tolist()[0]
+        plot2Vars(figName, (20, 15), "Total burned area in a year", [2, 2, 3],
+                  totalAreaDF["YEAR_"], totalAreaDF[cause], "Year",
+                  "Total burned area per year (ha)", markers=markerFmt,
+                  labelLegend=cause)
+        if regressDisp == "Y":
+            m = causesDescribed.loc[ind, "m_totalBurnedArea"]
+            b = causesDescribed.loc[ind, "b_totalBurnedArea"]
+            predicted = m*years + b
+            py.plot(totalAreaDF["YEAR_"], predicted, markerFmt, label=cause)
+    # py.savefig(figName)
+    return
+
+
+# Plotting data for whole data period
+'''
+plotCauses(["Lightning", "Arson"], "Y", "N",
+           "Analyzing Lightning & Arson", "all")
+manMadeCauses = causesColumns[1:8] + causesColumns[9:13] + causesColumns[14:18]
+plotCauses(["Lightning", "Manmade"], "Y", "N",
+           "Analyzing Lightning vs Manmade fires", "all")
+plotCauses(manMadeCauses, "Y", "N", "Comparing individual manmade causes",
+           "all")
+plotCauses(["Equipment Use", "Arson", "Vehicle", "Power Line", "Manmade"], "N",
+           "N", "Major manmade causes", "all",
+           ["Campfire", "Arson", "Manmade"],
+           ["Equipment Use", "Campfire", "Arson", "Manmade"])
+
+# Plotting recent data from 1950 onwards
+plotCauses(["Lightning", "Arson"], "Y", "N",
+           "Analyzing Lightning & Arson - 1950 to present", "recent")
+plotCauses(["Lightning", "Manmade"], "Y", "N",
+           "Analyzing Lightning vs Manmade fires - 1950 to present", "recent")
+plotCauses(manMadeCauses, "Y", "N",
+           "Comparing individual manmade causes - 1950 to present", "recent")
+plotCauses(["Equipment Use", "Arson", "Vehicle", "Power Line", "Manmade"], "N",
+           "N", "Major manmade causes - 1950 to present", "recent",
+           ["Campfire", "Arson", "Manmade"],
+           ["Equipment Use", "Campfire", "Arson", "Manmade"])'''
